fbedab308276d060d5e09a0dbc7684369638eb2a
Sean Hogg <seanhogg@gmail.com>
2025-01-19 10:50:16 -0500

    MYV-104 Release Notes & Contact Us

--- Client/Layout/MainLayout.razor.cs
+++ Client/Layout/MainLayout.razor.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
@@ -8,14 +9,46 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.JSInterop;
+using MyVideoResume.Client.Pages.App.People.Resumes;
 using MyVideoResume.Client.Shared;
 using Radzen;
 using Radzen.Blazor;
+using Spire.Doc;
 
 namespace MyVideoResume.Client.Layout;
 
-public partial class MainLayout 
+public partial class MainLayout
 {
+    [Inject] ThemeService ThemeService { get; set; }
+
+    const string logodark = "images/logo-login.png";
+    const string logolight = "images/logo-login-dark.png";
+    public string Logo { get; set; } = logolight;
+
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+        OnThemeChanged();
+        ThemeService.ThemeChanged += OnThemeChanged;
+    }
+
+    public void Dispose()
+    {
+        ThemeService.ThemeChanged -= OnThemeChanged;
+    }
+
+    void OnThemeChanged()
+    {
+        switch (ThemeService.Theme)
+        {
+            case "standard-dark":
+                Logo = logolight;
+                break;
+            case "standard":
+                Logo = logodark;
+                break;
+        }
+    }
     protected void ProfileMenuClick(RadzenProfileMenuItem args)
     {
         if (args.Value == "Logout")
