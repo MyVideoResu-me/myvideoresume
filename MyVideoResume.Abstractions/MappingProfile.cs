8fba0e167a321eb30b6b3ab1c2714aacbef3d441
Sean Hogg <seanhogg@gmail.com>
2025-01-22 23:29:07 -0500

    MYV-117 Bug Fixing for Export of JSON

--- MyVideoResume.Abstractions/MappingProfile.cs
+++ MyVideoResume.Abstractions/MappingProfile.cs
@@ -7,8 +7,20 @@ public partial class MappingProfile : Profile
 {
     public MappingProfile()
     {
-        CreateMap<JSONResume, ExportJSONResume>().ReverseMap();
-        CreateMap<ExportJSONResume, JSONResume>();
         CreateMap<JSONResume, ExportJSONResume>();
+        CreateMap<Basics, ExportBasics>();
+        CreateMap<Work, ExportWork>();
+        CreateMap<Volunteer, ExportVolunteer>();
+        CreateMap<Education, ExportEducation>();
+        CreateMap<Award, ExportAward>();
+        CreateMap<Certificate, ExportCertificate>();
+        CreateMap<Publication, ExportPublication>();
+        CreateMap<Skill, ExportSkill>();
+        CreateMap<LanguageItem, ExportLanguageItem>();
+        CreateMap<Interest, ExportInterest>();
+        CreateMap<ReferenceItem, ExportReferenceItem>();
+        CreateMap<Project, ExportProject>();
+        CreateMap<Location, ExportLocation>();
+        
     }
 }
