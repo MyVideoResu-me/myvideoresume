eeb42beaf1a69bb310e943d73655510221793d0d
Sean Hogg <seanhogg@gmail.com>
2025-01-30 19:25:47 -0500

    MYV-101 Wired Up

--- MyVideoResume.Workers/WorkerJobs.cs
+++ MyVideoResume.Workers/WorkerJobs.cs
@@ -98,10 +98,12 @@ public class RecurringJobsService : BackgroundService
             //_recurringJobs.AddOrUpdate("Send", () => Console.WriteLine("Hello, seconds!"), "*/15 * * * * *");
             //_recurringJobs.AddOrUpdate("SemanticScoring", () => _resumeService.ProcessSemanticScore(), "0 */8 * * *"); //Every 8 Hours
             _recurringJobs.AddOrUpdate("SemanticScoring", () => _resumeService.ProcessSemanticScore(), "0 */8 * * *"); //Daily 8AM
-            //_recurringJobs.AddOrUpdate("CrawlWebsiteCreateJobs-SimplyHired-Developer", () => _jobService.CrawlWebsiteCreateJobs("https://www.simplyhired.com/search?q=Developer&l="), "0 8 * * *"); //Daily 8AM
-            //_recurringJobs.AddOrUpdate("CrawlWebsiteCreateJobs-Jora-Careers", () => _jobService.CrawlWebsiteCreateJobs("https://us.jora.com/j?sp=search&trigger_source=serp&q=Software+Engineer&l="), "0 */4 * * *"); //Every 4 hours
+                                                                                                                       //_recurringJobs.AddOrUpdate("CrawlWebsiteCreateJobs-SimplyHired-Developer", () => _jobService.CrawlWebsiteCreateJobs("https://www.simplyhired.com/search?q=Developer&l="), "0 8 * * *"); //Daily 8AM
+#if DEBUG
             _recurringJobs.AddOrUpdate("CrawlWebsiteCreateJobs-Jora-Careers", () => _jobService.CrawlWebsiteCreateJobs("https://us.jora.com/j?sp=search&trigger_source=serp&q=Software+Engineer&l="), Cron.Minutely); //Minutely
-
+#else
+            _recurringJobs.AddOrUpdate("CrawlWebsiteCreateJobs-Jora-Careers", () => _jobService.CrawlWebsiteCreateJobs("https://us.jora.com/j?sp=search&trigger_source=serp&q=Software+Engineer&l="), "0 */4 * * *"); //Every 4 hours
+#endif
 
             //_recurringJobs.AddOrUpdate("SemanticScoring", () => _resumeService.ProcessSemanticScore(), Cron.Minutely); //Minutely
             //_recurringJobs.AddOrUpdate("SemanticScoring", () => _resumeService.ProcessSemanticScore(), "25 15 * * *"); //Hourly
