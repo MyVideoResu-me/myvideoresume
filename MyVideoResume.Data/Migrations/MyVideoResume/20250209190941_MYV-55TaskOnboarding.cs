37cd7cdbca962a378dde528067d2d1598267aaaa
Sean Hogg <seanhogg@gmail.com>
2025-02-09 14:10:31 -0500

    MYV-55 Onboarding Tasks

---
+++ MyVideoResume.Data/Migrations/MyVideoResume/20250209190941_MYV-55TaskOnboarding.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyVideoResume.Data.Migrations.MyVideoResume
+{
+    /// <inheritdoc />
+    public partial class MYV55TaskOnboarding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ActionToTake",
+                table: "Tasks",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ActionToTake",
+                table: "Tasks");
+        }
+    }
+}
