67b92db04a08a0ddc59cc5941f766513e2b46f59
Sean Hogg <seanhogg@gmail.com>
2025-02-18 01:33:25 -0500

    MYV-139 Fixing Email

--- MyVideoResume.Tests/AccountControllerTests.cs
+++ MyVideoResume.Tests/AccountControllerTests.cs
@@ -51,53 +51,12 @@ namespace MyVideoResume.Server.Controllers.Tests
 
             envMock = new Mock<IWebHostEnvironment>();
             loggerMock = new Mock<ILogger<AccountController>>();
-            emailServiceMock = new Mock<IEmailService>(new Mock<IConfiguration>().Object, new Mock<ILogger<IEmailService>>().Object);
+            emailServiceMock = new Mock<IEmailService>();
             configurationMock = new Mock<IConfiguration>();
             accountServiceMock = new Mock<AccountService>(new Mock<DataContext>().Object, new Mock<ILogger<AccountService>>().Object, new Mock<IMapper>().Object, userManagerMock.Object, roleManagerMock.Object, new Mock<TaskService>(new Mock<DataContext>().Object, new Mock<ILogger<AccountService>>().Object, new Mock<IMapper>().Object).Object);
 
             controller = new AccountController(envMock.Object, signInManagerMock.Object, userManagerMock.Object, roleManagerMock.Object, loggerMock.Object, emailServiceMock.Object, new Mock<DataContextService>(new Mock<DataContext>().Object, new Mock<NavigationManager>().Object).Object, configurationMock.Object, accountServiceMock.Object);
         }
-
-        //[Fact]
-        //public async Task GenerateToken_ValidUser_ReturnsToken()
-        //{
-        //    // Arrange
-        //    var userName = "testuser";
-        //    var password = "password";
-        //    var user = new ApplicationUser { UserName = userName };
-        //    userManagerMock.Setup(um => um.FindByNameAsync(userName)).ReturnsAsync(user);
-        //    userManagerMock.Setup(um => um.CheckPasswordAsync(user, password)).ReturnsAsync(true);
-        //    configurationMock.Setup(c => c["Jwt:Key"]).Returns("supersecretkey");
-        //    configurationMock.Setup(c => c["Jwt:Issuer"]).Returns("issuer");
-        //    configurationMock.Setup(c => c["Jwt:Audience"]).Returns("audience");
-
-        //    // Act
-        //    var result = await controller.GenerateToken(userName, password);
-
-        //    // Assert
-        //    var okResult = Assert.IsType<OkObjectResult>(result);
-        //    Assert.NotNull(okResult.Value);
-        //}
-
-        //[Fact]
-        //public async Task Login_ValidUser_ReturnsRedirect()
-        //{
-        //    // Arrange
-        //    var userName = "testuser";
-        //    var password = "password";
-        //    var redirectUrl = "~/";
-        //    var user = new ApplicationUser { UserName = userName, Email = userName, EmailConfirmed = true };
-        //    userManagerMock.Setup(um => um.FindByNameAsync(userName)).ReturnsAsync(user);
-        //    userManagerMock.Setup(um => um.GetTwoFactorEnabledAsync(user)).ReturnsAsync(false);
-        //    signInManagerMock.Setup(sm => sm.PasswordSignInAsync(userName, password, false, false)).ReturnsAsync(Microsoft.AspNetCore.Identity.SignInResult.Success);
-
-        //    // Act
-        //    var result = await controller.Login(userName, password, redirectUrl);
-
-        //    // Assert
-        //    var redirectResult = Assert.IsType<RedirectResult>(result);
-        //    Assert.Equal(redirectUrl, redirectResult.Url);
-        //}
 
         [Fact]
         public async Task Login_InvalidUser_ReturnsRedirectWithError()
@@ -115,35 +74,6 @@ namespace MyVideoResume.Server.Controllers.Tests
             var redirectResult = Assert.IsType<RedirectResult>(result);
             Assert.Contains("error=Invalid user or password", redirectResult.Url);
         }
-
-        //[Fact]
-        //public async Task VerifySecurityCode_ValidCode_ReturnsRedirect()
-        //{
-        //    // Arrange
-        //    var code = "123456";
-        //    var redirectUrl = "~/";
-        //    signInManagerMock.Setup(sm => sm.TwoFactorSignInAsync("Email", code, false, false)).ReturnsAsync(Microsoft.AspNetCore.Identity.SignInResult.Success);
-        //    var userId = "testuser";
-        //    var user = new ApplicationUser { Id = userId };
-        //    userManagerMock.Setup(um => um.FindByIdAsync(userId)).ReturnsAsync(user);
-        //    controller.ControllerContext = new ControllerContext
-        //    {
-        //        HttpContext = new DefaultHttpContext
-        //        {
-        //            User = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
-        //            {
-        //                new Claim(ClaimTypes.NameIdentifier, userId)
-        //            }))
-        //        }
-        //    };
-
-        //    // Act
-        //    var result = await controller.VerifySecurityCode(code, redirectUrl);
-
-        //    // Assert
-        //    var redirectResult = Assert.IsType<RedirectResult>(result);
-        //    Assert.Equal(redirectUrl, redirectResult.Url);
-        //}
 
         [Fact]
         public async Task VerifySecurityCode_InvalidCode_ReturnsRedirectWithError()
@@ -217,32 +147,6 @@ namespace MyVideoResume.Server.Controllers.Tests
             Assert.Equal("Error", badRequestResult.Value);
         }
 
-        //[Fact]
-        //public async Task CurrentUser_ReturnsAuthenticationState()
-        //{
-        //    // Arrange
-        //    var userId = "testuser";
-        //    var user = new ApplicationUser { Id = userId };
-        //    userManagerMock.Setup(um => um.FindByIdAsync(userId)).ReturnsAsync(user);
-        //    controller.ControllerContext = new ControllerContext
-        //    {
-        //        HttpContext = new DefaultHttpContext
-        //        {
-        //            User = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
-        //            {
-        //                new Claim(ClaimTypes.NameIdentifier, userId)
-        //            }))
-        //        }
-        //    };
-
-        //    // Act
-        //    var result = await controller.CurrentUser();
-
-        //    // Assert
-        //    Assert.NotNull(result);
-        //    Assert.Equal(userId, result.Name);
-        //}
-
         [Fact]
         public async Task Logout_ReturnsRedirect()
         {
@@ -253,41 +157,6 @@ namespace MyVideoResume.Server.Controllers.Tests
             var redirectResult = Assert.IsType<RedirectResult>(result);
             Assert.Equal("~/", redirectResult.Url);
         }
-
-        //[Fact]
-        //public async Task Register_ValidUser_ReturnsOk()
-        //{
-        //    // Arrange
-        //    var userName = "testuser";
-        //    var password = "password";
-        //    var user = new ApplicationUser { UserName = userName, Email = userName };
-        //    userManagerMock.Setup(um => um.CreateAsync(user, password)).ReturnsAsync(IdentityResult.Success);
-        //    roleManagerMock.Setup(rm => rm.Roles).Returns(new List<ApplicationRole> { new ApplicationRole { Name = "AccountAdmin" }, new ApplicationRole { Name = "AccountOwner" } }.AsQueryable());
-
-        //    // Act
-        //    var result = await controller.Register(userName, password);
-
-        //    // Assert
-        //    Assert.IsType<OkResult>(result);
-        //}
-
-        //[Fact]
-        //public async Task Register_InvalidUser_ReturnsBadRequest()
-        //{
-        //    // Arrange
-        //    var userName = "testuser";
-        //    var password = "password";
-        //    var user = new ApplicationUser { UserName = userName, Email = userName };
-        //    userManagerMock.Setup(um => um.CreateAsync(user, password)).ReturnsAsync(IdentityResult.Failed(new IdentityError { Description = "Error" }));
-        //    roleManagerMock.Setup(rm => rm.Roles).Returns(new List<ApplicationRole> { new ApplicationRole { Name = "AccountAdmin" }, new ApplicationRole { Name = "AccountOwner" } }.AsQueryable());
-
-        //    // Act
-        //    var result = await controller.Register(userName, password);
-
-        //    // Assert
-        //    var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
-        //    Assert.Equal("Error", badRequestResult.Value);
-        //}
 
         [Fact]
         public async Task ConfirmEmail_ValidCode_ReturnsRedirect()
@@ -324,22 +193,6 @@ namespace MyVideoResume.Server.Controllers.Tests
             var redirectResult = Assert.IsType<RedirectResult>(result);
             Assert.Contains("error=Invalid user or confirmation code", redirectResult.Url);
         }
-
-        //[Fact]
-        //public async Task ResetPassword_ValidUser_ReturnsOk()
-        //{
-        //    // Arrange
-        //    var userName = "testuser";
-        //    var user = new ApplicationUser { UserName = userName, Email = userName };
-        //    userManagerMock.Setup(um => um.FindByNameAsync(userName)).ReturnsAsync(user);
-        //    userManagerMock.Setup(um => um.GeneratePasswordResetTokenAsync(user)).ReturnsAsync("code");
-
-        //    // Act
-        //    var result = await controller.ResetPassword(userName);
-
-        //    // Assert
-        //    Assert.IsType<OkResult>(result);
-        //}
 
         [Fact]
         public async Task ResetPassword_InvalidUser_ReturnsBadRequest()
