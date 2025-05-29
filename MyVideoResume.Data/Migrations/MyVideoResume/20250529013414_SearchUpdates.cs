9a9038ee693ded40f16217c8c5c0f8ceed1e0d19
Sean Hogg <seanhogg@gmail.com>
2025-05-28 21:35:48 -0400

    fixing models

---
+++ MyVideoResume.Data/Migrations/MyVideoResume/20250529013414_SearchUpdates.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyVideoResume.Data.Migrations.MyVideoResume
+{
+    /// <inheritdoc />
+    public partial class SearchUpdates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DistanceUnitPreference",
+                table: "UserProfiles",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Latitude",
+                table: "UserProfiles",
+                type: "float",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Longitude",
+                table: "UserProfiles",
+                type: "float",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "AssignedToUserId",
+                table: "Tasks",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "AssignedToUserId",
+                table: "Projects",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "AssignedToUserId",
+                table: "Appointments",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DistanceUnitPreference",
+                table: "UserProfiles");
+
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "UserProfiles");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "UserProfiles");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "AssignedToUserId",
+                table: "Tasks",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AssignedToUserId",
+                table: "Projects",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AssignedToUserId",
+                table: "Appointments",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+        }
+    }
+}
